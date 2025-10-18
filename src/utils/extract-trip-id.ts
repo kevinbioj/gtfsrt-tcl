259ded6ce8922b07c9f2afe9ebe59be6b54d208e
Kevin BIOJOUT <biojoutk1106@gmail.com>
2025-10-18 13:43:06 +0200

    no more need to slice journey ids

--- src/utils/extract-trip-id.ts
+++ src/utils/extract-trip-id.ts
@@ -2,6 +2,5 @@ import { parseSiriRef } from "./parse-siri.js";
 
 export const extractTripId = (input: string) => {
 	const fixedInput = input.replaceAll("_x0040_", "@").replaceAll("_x00A3_", "£");
-	const journeyRef = parseSiriRef(fixedInput);
-	return journeyRef?.slice(journeyRef?.indexOf("@") + 1);
+	return parseSiriRef(fixedInput);
 };
